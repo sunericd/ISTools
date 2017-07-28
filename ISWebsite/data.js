babc71ef646b4fb5c4767b47a39a53051bc04413
1004parky <1004parky@gmail.com>
2017-07-27 20:56:22 -0400

    Website now marks on navigation which page you're on

--- ISWebsite/data.js
+++ ISWebsite/data.js
@@ -1,22 +1,42 @@
-function doOnClick(myObj) {
-	if (typeof console == "object") {
-		console.log("A 'click' event occured"); 
-		console.log(myObj);
-	}
-	$(document.getElementById('success').style.display = "block")
-}
-/* 
-function startSlides(start, end, delay) {
-    setTimeout(slideshow(start,start,end, delay), delay);
-}
+var nav_url = "https://raw.githubusercontent.com/sunericd/ISTools/master/ISWebsite/nav.json";
 
-function slideshow(frame, start, end, delay) {
-    return (function() {
-    $(document.getElementByClassName('.toolSlide'+ frame).fadeOut());
-    if (frame == end) { frame = start; } else { frame += 1; }
-    setTimeout(function(){$(document.getElementByClassName('.toolSlide'+ frame).fadeIn());}, 500);
-    setTimeout(slideshow(frame, start, end, delay), delay + 500);
+var tools_url = "https://raw.githubusercontent.com/sunericd/ISTools/master/ISWebsite/tools.json";
+
+var dev_url="https://raw.githubusercontent.com/sunericd/ISTools/master/ISWebsite/developers.json";
+
+
+$(document).ready(function(){
+
+  /* Handlebars for navigation */
+  $.getJSON(nav_url,
+    function(data){
+      var mysource = $('.nav').html();
+      var mytemplate = Handlebars.compile(mysource);
+      var myresult = mytemplate(data);
+      $('.nav').html(myresult);
+
+      /* Also distinguish current page */
+      var mymainid = $('main').attr('id');
+      var mynavid = '#navlink'+mymainid;
+      $(mynavid).attr('id','iamhere');
     })
-}
 
-startSlides(1, 4, 2000) */
+  /* Handlebars for tools on index.html*/
+  $.getJSON(tools_url,
+    function(data){
+      var mysource = $('.tools_info').html();
+      var mytemplate = Handlebars.compile(mysource);
+      var myresult = mytemplate(data);
+      $('.tools_info').html(myresult);
+    })
+
+    /* Handlebars for developers on About Us */
+    $.getJSON(dev_url,
+      function(data){
+        var mysource = $('.person').html();
+        var mytemplate = Handlebars.compile(mysource);
+        var myresult = mytemplate(data);
+        $('.person').html(myresult);
+      })
+    
+})
