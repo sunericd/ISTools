2fd59d98e59bb7ad5a7dd48541ce084334de2df9
Eric Sun <Eric Sun>
2017-08-04 12:55:27 -0400

    Added test framework

---
+++ ISToolkit/testall.py
@@ -0,0 +1,16 @@
+# Testing for the whole ISgeneTK package
+
+import mods.gel_visualizer
+import mods.SeqProp
+import mods.plasmid_builder
+import mods.MSM
+
+def test_GelViz():
+# break up into a lengths, and pic function, with a main fcn
+
+def test_SeqProp():
+# break up into smaller functions and then assert for each
+
+def test_BUILDR():
+
+def test_MSM():
