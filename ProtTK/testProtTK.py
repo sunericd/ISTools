5cae81d1fe70a41056c3ef7ceb3aa1cb10fa2f81
sunericd <edsun97@gmail.com>
2017-08-15 16:39:41 -0400

    Added testing data and values for ProtQuant

--- ProtTK/testProtTK.py
+++ ProtTK/testProtTK.py
@@ -1,6 +1,7 @@
 # Testing for the whole ISProtTK package
 
 import mods.ProtQuant as pq
+import numpy.testing as npt
 
 def setup_module(module):
     print ("") # this is to get a newline after the dots
@@ -40,20 +41,21 @@ class TestProt:
 	def test_ProtQuant(self):
 		print ("Testing ProtQuant...")
 		# Testing readStandards()
-		standard_concs, standard_fluors = pq.readStandards(FILENAME)
-		assert standard_concs[0] == VAL, "Error reading standard concentrations."
-		assert standard_fluors[0][0] == VAL, "Error reading standard data."
+		standard_concs, standard_fluors = pq.readStandards('Test_Data/test_standards.csv')
+		assert standard_concs[0] == 2, "Error reading standard concentrations."
+		assert standard_fluors[0][0] == 4, "Error reading standard data."
 		# Testing readProteins()
-		prot_fluors = pq.readProteins(FILENAME)
-		assert prot_fluors[0][0] == VAL, "Error reading protein data."
+		prot_fluors = pq.readProteins('Test_Data/test_proteins.csv')
+		assert prot_fluors[0][0] == 3, "Error reading protein data."
 		# Testing averageRows()
-		assert pq.averageRows(standard_fluors) == VAL, "Error with averaging standard_fluors. Check the test data."
-		assert pq.averageRows(prot_fluors) == VAL, "Error with averaging prot_fluors. Check the test data."
+		assert pq.averageRows(standard_fluors) == [4, 2, 1, 0.5, 0.25], "Error with averaging standard_fluors. Check the test data."
+		assert pq.averageRows(prot_fluors) == [4, 2, 1, 0.5], "Error with averaging prot_fluors. Check the test data."
 		# Testing regressor()
 		prot_concs = pq.regressor(standard_concs, standard_fluors, prot_fluors, 'Linear')
-		assert prot_concs[0] == VAL, "Error: Regression value is off for protein #1"
-		assert prot_concs[1] == VAL, "Error: Regression value is off for protein #2"
-		assert prot_concs[2] == VAL, "Error: Regression value is off for protein #3"
+		npt.assert_approx_equal(prot_concs[0], 2), "Error: Regression value is off for protein #1"
+		npt.assert_approx_equal(prot_concs[0], 1), "Error: Regression value is off for protein #2"
+		npt.assert_approx_equal(prot_concs[0], 0.5), "Error: Regression value is off for protein #3"
+		npt.assert_approx_equal(prot_concs[0], 0.25), "Error: Regression value is off for protein #4"
 
 
 
